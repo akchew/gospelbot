91e15b81b280ba75527e65ce8d78482d97f2629b
Geoffrey Goh <kg422@cornell.edu>
2016-10-22 21:20:19 -0700

    add api endpoint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-server.post('/', connector.listen());
+server.post('/api', connector.listen());
 //var connector = new builder.ConsoleConnector().listen();
 
 var bot = new builder.UniversalBot(connector);
